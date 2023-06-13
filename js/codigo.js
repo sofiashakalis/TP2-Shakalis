e425e84ef08d5b42c081cc7472aa87473129d471
sofiashakalis <91572830+sofiashakalis@users.noreply.github.com>
2023-06-13 08:29:31 -0300

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,14 @@
+document.getElementById('menu').addEventListener('click', function () {
+
+    document.getElementById('navega').classList.toggle('mostrar');
+
+
+})
+
+lightbox.option({
+    'wrapAround': true,
+    'albumLabel': "Imagen %1 de %2",
+    'fadeDuration': 2000,
+    'maxWidth': 1000,
+    'maxHeight': 1000
+})
